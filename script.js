7adccb65335f3d03dd33b54ee4317dc0154ff639
dikachi <andrewnwuko@gmail.com>
2023-08-10 21:30:00 +0100

    third push

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+// function openNav() {
+//   document.getElementById("mySidenav").style.width = "250px";
+//   document.getElementById("main").style.marginLeft = "250px";
+// }
+
+// function closeNav() {
+//   document.getElementById("mySidenav").style.width = "0";
+//   document.getElementById("main").style.marginLeft= "0";
+// }
+ function find_max(nums) {
+   let max_num = Number.NEGATIVE_INFINITY; // smaller than all other numbers
+   for (let num of nums) {
+   if (num > max_num) {
+      // (Fill in the missing line here)
+    
+    }
+    
+  }
+   return max_num;
+  
 }
 
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
-}
+console.log('find_max');
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode:'class',
-  content: ["./src/index.html,input.css,script.js"],
+  content: ["/index.html,input.css,script.js"],
   theme: {
     spacing: {
       97: '200px'},
